50df76d9e3217ecb68cacb083c83f1733a257c6d
Ananya <59766849+AnanyaThapliyal2303@users.noreply.github.com>
2021-07-03 05:12:41 +0530

    Delete emailkey.js

--- emailkey.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import{ init } from 'emailjs-com';
-init("user_5CZ2p7y5NryVd84AQesAp");
-
-
-export default {
-    USER_ID: `user_5CZ2p7y5NryVd84AQesAp`, //userID
-    TEMPLATE_ID: `template_jy1y16p`,  //templateID
-    }
